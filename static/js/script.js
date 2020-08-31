b0e34b9211b1c20e3e14ec1563c524617619f850
Michael Koller <mkoller@Michaels-MacBook-Air.local>
2020-08-31 17:57:05 -0600

    final version

---
+++ static/js/script.js
@@ -0,0 +1,64 @@
+function rpsGame(yourChoice) {
+    let humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randomNumber());
+    results = decideWinner(humanChoice, botChoice);
+    message = finalMessage(results);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice, botChoice) {
+    let rpsDatabase = {
+        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+        'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
+    };
+
+    let yourScore = rpsDatabase[yourChoice][botChoice];
+    let botScore = rpsDatabase[botChoice][yourChoice];
+
+    return [yourScore, botScore];
+}
+
+function finalMessage([yourScore, botScore]) {
+    if (yourScore ===0) {
+        return {'message': 'You lost!', 'color': 'red'};
+    } else if (yourScore === 0.5) {
+        return {'message': 'You tied!', 'color': 'yellow'};
+    } else {
+        return {'message': 'You won!', 'color': 'green'};
+    }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    let imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    }
+
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    let humanDiv = document.createElement('div');
+    let botDiv = document.createElement('div');
+    let messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "'height=150 width=150 style='box-shadow: 5px 10px 8px #888888;'>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "'height=150 width=150 style='box-shadow: 5px 10px 8px #888888;'>"
+
+
+    document.getElementById('flex-box-rps').appendChild(humanDiv);
+    document.getElementById('flex-box-rps').appendChild(messageDiv);
+    document.getElementById('flex-box-rps').appendChild(botDiv);
+}
